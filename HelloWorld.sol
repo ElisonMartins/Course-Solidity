37bdecc816227481b7198f5fe9c0b46d6f1b3264
elisonmartins <martinselison10@gmail.com>
2024-01-24 15:55:29 -0300

    feat: Adding smart contract with helloworld

---
+++ HelloWorld.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    // Função para obter a mensagem "Hello World"
+    function getHelloWorld() public pure returns (string memory) {
+        return "Hello World";
+    }
+}
